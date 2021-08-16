668172a3d3824ce048248e2ccc08132ca7674e4f
arditmirashi2 <arditmirashi5@gmail.com>
2021-08-16 11:17:03 +0300

    Added ideal camera sizes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 window.onload = () => {
     const select = window.prompt("Select a camera mode [user, environment]")
 
@@ -8,8 +7,18 @@ window.onload = () => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         const constraints = {
             video: {
+                width: {
+                  min: 1280,
+                  ideal: 1920,
+                  max: 2560,
+                },
+                height: {
+                  min: 720,
+                  ideal: 1080,
+                  max: 1440
+                },
                 facingMode: select
-            },
+              },
             audio: false
         }
 
