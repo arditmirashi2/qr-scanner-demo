3a2fb8a2f37d2cb9c08192356cacf03a936da2be
arditmirashi2 <arditmirashi5@gmail.com>
2021-08-16 11:46:59 +0300

    Another commit

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// window.onload = () => {
-//     const select = window.prompt("Select a camera mode [user, environment]")
+window.onload = () => {
+    const select = window.prompt("Select a camera mode [user, environment]")
 
 
-//     const video = document.getElementById("scanner");
+    const video = document.getElementById("scanner");
 
-//     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-//         const constraints = {
-//             video: {
-//                 width: {
-//                   min: 1280,
-//                   ideal: 1920,
-//                   max: 2560,
-//                 },
-//                 height: {
-//                   min: 720,
-//                   ideal: 1080,
-//                   max: 1440
-//                 },
-//                 facingMode: select
-//               },
-//             audio: false
-//         }
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const constraints = {
+            video: {
+                width: {
+                  min: 1280,
+                  ideal: 1920,
+                  max: 2560,
+                },
+                height: {
+                  min: 720,
+                  ideal: 1080,
+                  max: 1440
+                },
+                facingMode: select
+              },
+            audio: false
+        }
 
-//         navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream);
-//     }
-// }
+        navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream);
+    }
+}
