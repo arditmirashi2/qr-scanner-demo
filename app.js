4ebacd397e7677d70b01b4b4ef02a0db6e3dabaf
arditmirashi2 <arditmirashi5@gmail.com>
2021-08-16 11:30:35 +0300

    Another commit

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 window.onload = () => {
+    const select = window.prompt("Select a camera mode [user, environment]")
+
+
     const video = document.getElementById("scanner");
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         const constraints = {
-            video: true,
+            video: {
+                width: {
+                  min: 1280,
+                  ideal: 1920,
+                  max: 2560,
+                },
+                height: {
+                  min: 720,
+                  ideal: 1080,
+                  max: 1440
+                },
+                facingMode: select
+              },
             audio: false
         }
 
